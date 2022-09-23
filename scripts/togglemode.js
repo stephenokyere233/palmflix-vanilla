6390b2931201d9e773f03e22c25cead59aef411c
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2022-09-23 10:02:57 +0000

    new fixes

--- scripts/togglemode.js
+++ scripts/togglemode.js
@@ -1,5 +1,15 @@
 const btn = document.querySelector(".fa-sun");
 const wrapper = document.querySelector(".wrapper");
+const menu = document.querySelectorAll(".hamburger");
+let tablet = document.querySelector("#tab");
+let expanded = document.querySelector("#expand");
+let showcase = document.querySelector(".showcase");
+let movieCard = document.querySelectorAll(".card");
+let moviePreview = document.querySelector("#movie-preview");
+let movieSelect = document.querySelector("#movies-select");
+let search = document.querySelector(".fa-magnifying-glass");
+let backBtn = document.querySelector(".back");
+
 const toggleMode = () => {
   document.body.classList.toggle("dark");
   if (btn.classList.contains("fa-sun")) {
@@ -12,48 +22,40 @@ const toggleMode = () => {
 };
 btn.addEventListener("click", toggleMode);
 
-const menu = document.querySelectorAll(".hamburger");
-// console.log(menu);
-let tablet = document.querySelector("#tab");
-let expanded = document.querySelector("#expand");
-// let width = window.innerWidth;
-let showcase = document.querySelector(".showcase");
-
 const hideElements = (e) => {
-  let clicked = false;
+  // let clicked = false;
   if (
     e.target.parentNode.id === "tab" ||
     e.target.classList.contains("hamburger") ||
     e.target.classList.contains("fa-bars")
   ) {
-    // console.log("a");
-    clicked = true;
+    // clicked = true;
     tablet.style.display = "none";
     expanded.style.display = "block";
+    movieSelect.classList.add("opacity");
+    moviePreview.classList.add("opacity");
+    wrapper.classList.add("disablescroll");
+    movieCard.forEach((element) => {
+      element.removeEventListener("click", showMoviePreview);
+    });
   } else {
-    // console.log("b");
     tablet.style.display = "flex";
     expanded.style.display = "none";
+    movieSelect.classList.remove("opacity");
+    moviePreview.classList.remove("opacity");
+    wrapper.classList.remove("disablescroll");
+    movieCard.forEach((element) => {
+      element.addEventListener("click", showMoviePreview);
+    });
   }
 };
 menu.forEach((element) => {
   element.addEventListener("click", hideElements);
 });
-window.addEventListener('click',(e)=>{
-  if(e.target===showcase){
-    expanded.style.display='none';
-    console.log('expansion');
-  }
-
-})
-
-let movieCard = document.querySelectorAll(".card");
-let moviePreview = document.querySelector("#movie-preview");
-let movieSelect = document.querySelector("#movies-select");
 
 const showMoviePreview = (e) => {
   if (e.target.parentNode.classList.contains("card")) {
-    console.log("found");
+    wrapper.scrollTo(0, 0);
     moviePreview.classList.remove("hide");
     movieSelect.classList.add("hide");
   }
@@ -61,8 +63,6 @@ const showMoviePreview = (e) => {
 movieCard.forEach((element) => {
   element.addEventListener("click", showMoviePreview);
 });
-// console.log(movieCard);
-let backBtn = document.querySelector(".back");
 const goBack = () => {
   moviePreview.classList.add("hide");
   movieSelect.classList.remove("hide");
@@ -70,19 +70,21 @@ const goBack = () => {
 
 backBtn.addEventListener("click", goBack);
 
-let search = document.querySelector(".fa-magnifying-glass");
-const openSearch=(e)=>{
-  if(e.target===search){
-     tablet.style.display = "none";
-     expanded.style.display = "block";
-    //  console.log('search');
+const dontShow = (e) => {
+  if (hideElements) {
+    if (e.target !== expanded) {
+      tablet.style.display = "flex";
+      expanded.style.display = "none";
+      movieSelect.classList.remove("opacity");
+      moviePreview.classList.remove("opacity");
+      wrapper.classList.remove("disablescroll");
+      movieCard.forEach((element) => {
+        element.addEventListener("click", showMoviePreview);
+      });
+    }
   }
+};
+movieSelect.addEventListener("click", dontShow);
+moviePreview.addEventListener("click", dontShow);
 
-}
-search.addEventListener('click',openSearch)
-
-// // test
-// import { add } from "./test.js";
-
-// // console.log(add(2,4));
-// btn.addEventListener('click',add(5,4))
+search.addEventListener("click", hideElements);
